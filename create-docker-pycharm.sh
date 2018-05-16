ca0cfc7756204c4046a2e4b946d7d71638d480c8
Sławomir Domagała <slawomir.karol.domagala@gmail.com>
2018-05-16 14:00:49 +0200

    More generic

--- create-docker-pycharm.sh
+++ create-docker-pycharm.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 cp "${BASH_SOURCE%/*}/docker-compose.override.yml" "$1/docker-compose.override.yml"
-cp "${BASH_SOURCE%/*}/.env.pycharm" "$1/.env.pycharm"
+
 DOCKERFILE_NAME="$1/docker/pycharm/Dockerfile"
+ENVFILE_NAME="$1/.env.pycharm"
+COMPOSE_PROJECT_NAME=`basename $1`
+
+cp "${BASH_SOURCE%/*}/.env.pycharm" $ENVFILE_NAME
+echo "UID=$UID" >> "$1/.env"
+mkdir -p "~/pycharm-docker/$COMPOSE_PROJECT_NAME/"
+
 mkdir -p "$1/docker/pycharm" && cp "${BASH_SOURCE%/*}/docker/pycharm/Dockerfile" $DOCKERFILE_NAME
-NAME=`basename $1`"_web"
-echo "FROM $NAME:latest" | cat - $DOCKERFILE_NAME | sponge $DOCKERFILE_NAME
-
-
+COMPOSE_WEB_NAME="$COMPOSE_PROJECT_NAME""_web"
+echo "FROM $COMPOSE_WEB_NAME:latest" | cat - $DOCKERFILE_NAME | sponge $DOCKERFILE_NAME
